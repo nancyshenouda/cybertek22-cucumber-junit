d27281874b0e6913884279240780da7424a605dc
nancyshenouda <Shenouda.nancy@gmail.com>
2021-07-17 16:14:15 -0400

    Wikipedia task

--- src/test/java/com/cybertek/step_definitions/Wiki_StepDefinitions.java
+++ src/test/java/com/cybertek/step_definitions/Wiki_StepDefinitions.java
@@ -1,29 +1,53 @@
 package com.cybertek.step_definitions;
 
+import com.cybertek.pages.WikiSearchPage;
+import com.cybertek.utilities.ConfigurationReader;
+import com.cybertek.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 public class Wiki_StepDefinitions {
+    WikiSearchPage object = new WikiSearchPage();
     @Given("User is on Wikipedia home page")
     public void user_is_on_wikipedia_home_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        String url = ConfigurationReader.getProperty("wikiUrl");
+        Driver.getDriver().get(url);
     }
-    @When("User types Steve Jobs in the wiki search box")
-    public void user_types_steve_jobs_in_the_wiki_search_box() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
+
     @When("User clicks wiki search button")
     public void user_clicks_wiki_search_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        object.searchButton.click();
+
     }
-    @Then("User sees Steve Jobs is in the wiki title")
-    public void user_sees_steve_jobs_is_in_the_wiki_title() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+    @When("User types {string} in the wiki search box")
+    public void userTypesInTheWikiSearchBox(String name) {
+        object.searchBox.sendKeys(name);
+
+    }
+
+    @Then("User sees {string} is in the main header")
+    public void userSeesIsInTheMainHeader(String name) {
+        String actualHeader = object.headerText.getText();
+        String expectedHeader = name;
+        Assert.assertEquals(expectedHeader, actualHeader);
+    }
+
+    @Then("User sees {string} is in the image header")
+    public void userSeesIsInTheImageHeader(String name) {
+String actualImageHeader = object.imageHeader.getText();
+String expectedImageHeader = name;
+Assert.assertEquals(expectedImageHeader,actualImageHeader);
+    }
+
+
+    @Then("User sees {string} is in the wiki title")
+    public void userSeesIsInTheWikiTitle(String name) {
+        String actualTitle = Driver.getDriver().getTitle();
+        String expectedTitle = name + " - Wikipedia";
+        Assert.assertTrue("Title verification failed!", actualTitle.equals(expectedTitle));
     }
 }
 
--- src/test/resources/features/WikiSearch.feature
+++ src/test/resources/features/WikiSearch.feature
@@ -2,9 +2,23 @@ Feature: Wikipedia Search Functionality
   As a user, I should be able to search terms and see relevant information
   Case: Search for "Steve Jobs", the title, the header, and the image header
   should be displayed as "Steve Jobs"
-  @wip
+
   Scenario: Wikipedia Search Functionality Title Verification
   Given User is on Wikipedia home page
-  When User types Steve Jobs in the wiki search box
+  When User types "Steve Jobs" in the wiki search box
   And User clicks wiki search button
-  Then User sees Steve Jobs is in the wiki title
+  Then User sees "Steve Jobs" is in the wiki title
+
+
+    Scenario: Wikipedia Search Functionality Header Verification
+    Given User is on Wikipedia home page
+    When User types "Steve Jobs" in the wiki search box
+    And User clicks wiki search button
+    Then User sees "Steve Jobs" is in the main header
+
+  @wip
+    Scenario: Wikipedia Search Functionality Image Header Verification
+      Given User is on Wikipedia home page
+      When User types "Steve Jobs" in the wiki search box
+      And User clicks wiki search button
+      Then User sees "Steve Jobs" is in the image header
